7bd899a9e7bceacbb0933f25b65ffdfbaed3a92b
Tyler Stanish <tystanish@gmail.com>
2018-08-01 11:05:32 +0200

    added some more tests

--- translate/tests.py
+++ translate/tests.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 
-from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
 
 from translate.views import translation
+from translate.models import TranslationEvent
 
 
 class TranslateTests(APITestCase):
@@ -11,11 +13,34 @@ class TranslateTests(APITestCase):
     def setUpTestData(cls):
         User.objects.create(email='tystanish@gmail.com', password='strongpassword')
 
-    def test_permission_translate_post(self):
+    def test_permission_translate_post_with_no_credentials(self):
         factory = APIRequestFactory()
         request = factory.post('/api/v1/translate/')
         response = translation(request)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_permission_translate_post_with_credentials(self):
+        # same as above but with a user
+        factory = APIRequestFactory()
+        user = User.objects.get(email='tystanish@gmail.com')
+        request = factory.post('/api/v1/translate/', {
+            'from_lang': 'en',
+            'to_lang': 'es',
+            'text': 'hello',
+            'will_practice': True
+        })
+        force_authenticate(request=request, user=user)
+        response = translation(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(TranslationEvent.objects.all()), 1)
 
     def test_permission_translate_get(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(email='tystanish@gmail.com')
+        request = factory.get('/api/v1/translate/')
+        force_authenticate(request=request, user=user)
+        response = translation(request=request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_create_translation(self):
         pass
